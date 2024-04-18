4e3342ffabc32aac4350f3312dade35d9ea37308
dunewalkin <sergei.gunchenko@gmail.com>
2024-04-19 00:04:41 +0400

    home page complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+const primaryNav = document.querySelector (".primary-navigation");
+const navToggle = document.querySelector (".mobile-nav-toggle");
+const headerWrapper = document.querySelector(".header-wrapper");
+
+navToggle.addEventListener('click', ()  => {
+   const visibility = headerWrapper.getAttribute("data-visible");
+   
+   if (visibility === "false") {
+      headerWrapper.setAttribute ("data-visible", "true");
+   } else if (visibility === "true") {
+      headerWrapper.setAttribute ("data-visible", "false");
+   }
+});
+
+navToggle.addEventListener('click', ()  => {
+   const visibility = primaryNav.getAttribute("data-visible");
+   
+   if (visibility === "false") {
+      primaryNav.setAttribute ("data-visible", true);
+      navToggle.setAttribute ("aria-expanded", true);
+   } else if (visibility === "true") {
+      primaryNav.setAttribute ("data-visible", false);
+      navToggle.setAttribute ("aria-expanded", false);
+   }
+}); 
+
 // Получаем элемент .how-it-works-list
 const howItWorksItem = document.querySelector('.how-it-works-item');
 
